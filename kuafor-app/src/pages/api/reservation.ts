e6caecd84e88736217aee7a3a86c598745ad1c33
kucukkanat <htolgasahin@gmail.com>
2023-02-21 22:08:37 +0100

    fix: api response and shit

--- kuafor-app/src/pages/api/reservation.ts
+++ kuafor-app/src/pages/api/reservation.ts
@@ -3,28 +3,25 @@ import {  Reservation, ReservationReq } from '@/entities'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { isEmptyBindingElement } from 'typescript'
 
-type Data = {
-  name: string
-}
-
 const reservations: Reservation[] = []
 
-
+function SaveReservation(params){
+  reservations.push(new Reservation({
+    customerID: params.customerID,
+    employeeID: params.employeeID,
+    start: params.start,
+  }))
+}
 export default function handler(
   req: ReservationReq,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Reservation[]>
 ) {
   if(req.method === "POST") {
-    console.log("\n\n\n\nParams:", req.body)
-    const params = JSON.parse(req.body)
     // Create new reservation
-    reservations.push(new Reservation({
-      customerID: params.customerID,
-      employeeID: params.employeeID,
-      start: params.start,
-    }))
+    SaveReservation(req.body)
   } else if (req.method === "GET") {
     // Return reservations
+    res.send(reservations)
   } else if (req.method === "PATCH") {
     // Update a reservation that already exists
   } else if (req.method === "DELETE") { 
